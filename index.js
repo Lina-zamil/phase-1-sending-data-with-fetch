df4b9e077bb8bd95e070e4132a0775ad98029cab
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-22 16:12:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({ "name": name, "email": email }),
+    }
+    const f = fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(data => document.body.innerHTML = data.id)
+        .catch(error => document.body.innerHTML = error.message);
+    return f
+}
+
